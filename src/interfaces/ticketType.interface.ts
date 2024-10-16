4b8ebdcfdc06c917baa267b4deaa3356d23ba111
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2024-10-16 12:01:32 -0300

    db: upate prisma schema

--- src/interfaces/ticketType.interface.ts
+++ src/interfaces/ticketType.interface.ts
@@ -3,7 +3,6 @@ export interface TicketTypeCreate {
 	description: string;
 	price: number;
 	quantity: number;
-	quantityAvailablePerUser: number;
 	salesStartDate?: Date;
 	salesEndDate?: Date;
 	isActive?: boolean;
@@ -15,7 +14,6 @@ export interface TicketType {
 	description: string;
 	price: number;
 	quantity: number;
-	quantityAvailablePerUser: number;
 	salesStartDate: Date | null;
 	salesEndDate: Date | null;
 	isActive: boolean;
